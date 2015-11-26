2737d10eb9388c53cf7ae28046611ff3f6fe8f18
root <root@iZ2838w57wgZ.(none)>
2015-11-26 21:14:50 +0800

    1511262114

--- ps.py
+++ ps.py
@@ -4,7 +4,7 @@ import os
 import sys
 import psutil
 def cpuinfo():
-	cpu_all=psutil.cpu_times_percent()
+	cpu_all=psutil.cpu_times_percent(interval=3)
 	cpu_idle=cpu_all.idle
 	print cpu_all
 	cpu_system=cpu_all.system
@@ -22,11 +22,20 @@ def diskinfo():
 		info1.append(psutil.disk_usage(disk_part).free)
 		info[disk_part]=info1
 	return info
+def meminfo():
+	mem_info=psutil.virtual_memory()
+	mem_total=mem_info.total/1024/1024
+	mem_used=mem_info.used/1024/1024
+	mem_free=mem_info.free/1024/1024
+	mem_images="The memory images is:\nmem_total:%6sM\tmem_used:%6sM\tmem_free:%6sM\n" % (mem_total,mem_used,mem_free)
+	return mem_images
 p=cpuinfo()
 f=diskinfo()
+m=meminfo()
 print p
 print "分区\tdisk_total\tdisk_used\tdisk_free"
 for k ,v in f.items():
 	for i in v:
 		k=k+"\t"+str(i/1024/1024)+"M"
 	print k
+print m
